ed0f0c96424aff9b917cfbd05b975fcca3f8b779
KDerevyankin <95866902+KDerevyankin@users.noreply.github.com>
2022-11-07 21:31:16 +0300

    Update sample_matrix.cpp

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -8,23 +8,151 @@
 #include "tmatrix.h"
 //---------------------------------------------------------------------------
 
+void rand_matrix(TDynamicMatrix<int>& Matrix)
+{
+    for (size_t i = 0; i < Matrix.size(); i++)
+    {
+        for (size_t j = 0; j < Matrix.size(); j++)
+        {
+            Matrix[i][j] = rand() % 100;
+        }
+    }
+}
+
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
+    int size, choice, operation;
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование класс работы с матрицами" << endl;
+    cout << "Выберите размер матрицы - ";
+    cin >> size;
+    TDynamicMatrix<int> Matrix1(size);
+    cout << "Если вы хотите заполнить матрицу самостоятельно напишите '1',\n если хотите заполнить матрицу случайными числами напишите '2'" << endl;
+    cin >> choice;
+    switch (choice)
     {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+    case 1:
+        cout << "Выберите операцию:\n1 - Произведение матриц\n2 - Сумма матриц\n3 - Разность матриц\n4 - Умножение матрицы на скаляр\n";
+        cin >> operation;
+        switch (operation)
+        {
+        case 1:
+        {
+            cout << "Введите первую матрицу\n";
+            cin >> Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            cout << "Введите вторую матрицу\n";
+            cin >> Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 * Matrix2;
+            break;
+        }
+        case 2:
+        {
+            cout << "Введите первую матрицу\n";
+            cin >> Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            cout << "Введите вторую матрицу\n";
+            cin >> Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 + Matrix2;
+            break;
+        }
+        case 3:
+        {
+            cout << "Введите первую матрицу\n";
+            cin >> Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            cout << "Введите вторую матрицу\n";
+            cin >> Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 - Matrix2;
+            break;
+        }
+        case 4:
+        {
+            cout << "Введите матрицу\n";
+            cin >> Matrix1;
+            int value;
+            cout << "Введите вектор\n";
+            cin >> value;
+            cout << "Результат\n";
+            cout << Matrix1 * value;
+            break;
+        }
+        default:
+        {
+            cout << "Неверный выбор операции\n";
+            break;
+        }
+        }
+        break;
+    case 2:
+        cout << "Выберите операцию:\n1 - Произведение матриц\n2 - Сумма матриц\n3 - Разность матриц\n4 - Умножение матрицы на скаляр\n";
+        cin >> operation;
+        switch (operation)
+        {
+        case 1:
+        {
+            rand_matrix(Matrix1);
+            cout << "Первая матрица\n";
+            cout << Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            rand_matrix(Matrix2);
+            cout << "Второая матрица\n";
+            cout << Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 * Matrix2;
+            break;
+        }
+        case 2:
+        {
+            rand_matrix(Matrix1);
+            cout << "Первая матрица\n";
+            cout << Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            rand_matrix(Matrix2);
+            cout << "Второая матрица\n";
+            cout << Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 + Matrix2;
+            break;
+        }
+        case 3:
+        {
+            rand_matrix(Matrix1);
+            cout << "Первая матрица\n";
+            cout << Matrix1;
+            TDynamicMatrix<int> Matrix2(size);
+            rand_matrix(Matrix2);
+            cout << "Второая матрица\n";
+            cout << Matrix2;
+            cout << "Результат\n";
+            cout << Matrix1 - Matrix2;
+            break;
+        }
+        case 4:
+        {
+            rand_matrix(Matrix1);
+            cout << "Первая матрица\n";
+            cout << Matrix1;
+            int value;
+            cout << "Введите вектор\n";
+            cin >> value;
+            cout << "Результат\n";
+            cout << Matrix1 * value;
+            break;
+        }
+        default:
+        {
+            cout << "Неверный выбор операции\n";
+            break;
+        }
+        }
+        break;
+    default:
+        cout << "Неверный выбор операции\n";
+        break;
     }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
 }
 //---------------------------------------------------------------------------
